4885465699f306f388a957d97b8fb5da07bb99ad
Erasmo Sanchez <90819217+OmareNas@users.noreply.github.com>
2025-05-15 12:08:39 -0500

    expand-dialogue
    Implemented a function named DisplayExtraLine to show additional dialogue if the player takes time to perform an action or progress in the game.

--- Assets/Narrator/NarratorManager.cs
+++ Assets/Narrator/NarratorManager.cs
@@ -13,7 +13,7 @@ public class NarratorManager : MonoBehaviour
 
 	[TextArea(3, 10)]
 	public string[] nmLines;
-	
+
 	public float textSpeed;
 	public int nmIndex;
 	public bool doneTalking;
@@ -21,46 +21,46 @@ public class NarratorManager : MonoBehaviour
 	public float pauseAfterLine = 1.5f;
 
 	// using narrator class
-	private string[] lines; 
+	private string[] lines;
 	public int index;
 	private bool pauseBttnTrig;
 	// public Narrator secondLines;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-       // lines = new Queue<string>();
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		// lines = new Queue<string>();
 		narratorText.text = string.Empty;
 		doneTalking = true;
 		nmIndex = 0;
 
 		StartNarratorManager();
-    }
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 		//Checck if narrator is done talking
-		if(doneTalking)
+		if (doneTalking)
 		{
-			Debug.Log("index : " +  index + " && lines.Length ; " + lines.Length);
+			Debug.Log("index : " + index + " && lines.Length ; " + lines.Length);
 
 			//just go to displaynextline
 			DisplayNextline();
 		}
-    }
+	}
 
-    public void StartNarratorManager ()
+	public void StartNarratorManager()
 	{
-		
+
 		bttnTrig = false;
 		// set up lines and index for each narrator option 
 		if (doneTalking)
 		{
 			lines = nmLines;
-			index = nmIndex; 
+			index = nmIndex;
 			doneTalking = false;
 
-		 	// DisplayElements(lines);
+			// DisplayElements(lines);
 
 			DisplayNextline();
 			nmIndex++;
@@ -68,50 +68,51 @@ public class NarratorManager : MonoBehaviour
 		}
 	}
 
-    public void StartNarrator(Narrator narrator) //()
-    {
-        bttnTrig = true;
+	public void StartNarrator(Narrator narrator) //()
+	{
+		bttnTrig = true;
 		// set up lines and index for each narrator option 
 		// if (doneTalking)
 		// {
-			// doneTalking = false;
-			lines = narrator.lines;
-			index = narrator.index;
+		// doneTalking = false;
+		lines = narrator.lines;
+		index = narrator.index;
 
-			// DisplayElements(narrator.lines);
+		// DisplayElements(narrator.lines);
 
-			StopAllCoroutines();
-			DisplayNextline();
-			// narrator.index++;
+		StopAllCoroutines();
+		DisplayNextline();
+		// narrator.index++;
 		// }
-    }
+	}
 
-    public void DisplayNextline ()
+	public void DisplayNextline()
 	{
-		Debug.Log("index in DPL: " + index);		
+		Debug.Log("index in DPL: " + index);
 		doneTalking = false;
 
-		if (index < (lines.Length) ) // 0 < (3), 1<(3), 2<(3) 
-        {
-            // index++;
-            narratorText.text = string.Empty;
+		if (index < (lines.Length)) // 0 < (3), 1<(3), 2<(3) 
+		{
+			// index++;
+			narratorText.text = string.Empty;
 			StopAllCoroutines();
-            StartCoroutine(Typeline());
-            // doneTalking = true;
-        }
-        else
+			StartCoroutine(Typeline());
+			// doneTalking = true;
+		}
+		else
 		{
 			Debug.Log("Extra Line");
-            // narratorText.text = "No More Lines";
-			doneTalking=true;
+			// narratorText.text = "No More Lines";
+			doneTalking = true;
 
-			
-		}	
+			DisplayExtraLine();
+
+		}
 
 	}
 
-    // IEnumerator Typeline (string line)
-	IEnumerator Typeline ()
+	// IEnumerator Typeline (string line)
+	IEnumerator Typeline()
 	{
 		Debug.Log("index in TL: " + index);
 		foreach (char c in lines[index].ToCharArray())
@@ -122,27 +123,26 @@ public class NarratorManager : MonoBehaviour
 		index++;
 
 		// wait for reader to finish reading
-        yield return new WaitForSeconds(pauseAfterLine);
+		yield return new WaitForSeconds(pauseAfterLine);
 
-		
-        
+
 		// if not trigger set bool to true
 		// if(!bttnTrig)
-		if(!pauseBttnTrig)
+		if (!pauseBttnTrig)
 			doneTalking = true;
-		
+
 
 	}
 
-    // Debug: Display the lines[] on unity console
-    private void DisplayElements(string[] lines)
+	// Debug: Display the lines[] on unity console
+	private void DisplayElements(string[] lines)
 	{
 		// Diplay all lines elements
-			foreach (string line in lines)
-			{
-				Debug.Log(line);
-			}
-		
+		foreach (string line in lines)
+		{
+			Debug.Log(line);
+		}
+
 	}
 
 	public void PauseTrigger(Narrator narrator)
@@ -152,10 +152,10 @@ public class NarratorManager : MonoBehaviour
 		// increment the index
 		// after it finish line clear text until next call
 		// check if the index is equal to three then reset it 
-		
+
 
 		// bttnTrig = true;
-		pauseBttnTrig= true;
+		pauseBttnTrig = true;
 		lines = narrator.lines;
 		index = narrator.index;
 		// StopAllCoroutines();
@@ -163,19 +163,19 @@ public class NarratorManager : MonoBehaviour
 		Debug.Log("PT Line [" + nmIndex + " ] is : " + lines[nmIndex]);
 
 		// check if pressed three times
-		
+
 
 		// narratorText.text = string.Empty;
 		doneTalking = false;
 		narrator.index++;
 
-		Debug.Log("Pause index : " + index );
+		Debug.Log("Pause index : " + index);
 
 		if (index == 2)
 		{
 			Debug.Log("Inside pause trigger if statement");
 			// bttnTrig=false;
-			pauseBttnTrig =false;
+			pauseBttnTrig = false;
 			narrator.index = 0;
 			narratorText.text = string.Empty;
 			doneTalking = true;
@@ -186,5 +186,58 @@ public class NarratorManager : MonoBehaviour
 
 	}
 
+	void DisplayExtraLine()
+	{
+		/*
+		*** Have an array of at least 10 lines
+		*** Randomize between the lines and choose one
+		*** Clear the narrator text and 
+		*** Display the chosen line 
+		*** 
+		*/
+		// const int sleLength = 10; 
+		// Added "..." and "   " for spacing
+		
+		string[] extraLines = 	
+					{"Still here? Wow, your patience is *really* something."
+					,"   "
+					,"You know, I'm starting to think you like doing nothing."
+					,"   "
+					,"Nothing happening? Well, you're not helping either, are you?"
+					,"...   "
+					,"Wow, not even a click? Are you just here to watch me talk?"
+					,"   "
+					,"Don't be shy. I know you want to click. What's stopping you?"
+					,"   "
+					,"   "
+					,"You're waiting for a sign? Here it is: *click something already.*"
+					,"   "
+					,"Is this your idea of fun? Just sitting there like a mannequin?"
+					,"...   "
+					,"   "
+					,"Look, I'm fine with this, but I'm *really* not sure you are."
+					,"   "
+					,"You've been here a while. Should I send you a participation trophy?"
+					,"   "
+					,"Still nothing? Well, I guess I'll just keep talking then..."};
+		int tmpIndex = 0;
+		string tmpStr = "";
+		string[] tmpLine = {""};
+		var random = new System.Random();
+		tmpIndex = random.Next(0,extraLines.Length);
+		Debug.Log("random int : " + tmpIndex);
+		Debug.Log("eL : " + extraLines[tmpIndex]);
+		tmpStr = extraLines[tmpIndex];
+		tmpLine[0] = tmpStr;
+		Debug.Log("tL : " + tmpLine[0]);
+
+		lines = tmpLine;
+		index=0;
+		narratorText.text = string.Empty;
+		StopAllCoroutines();
+		StartCoroutine(Typeline());
+
+
+	}
 	
 }
